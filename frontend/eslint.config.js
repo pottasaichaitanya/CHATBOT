d573e7b83e2102bc19f85f92d6ee6eef12f2d65d
Sai Chaitanya <saichaitanyapotta21@gmail.com>
2025-08-09 00:09:20 +0530

    added test cases

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,31 +1,43 @@
-export default {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+
+export default [
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        browser: true, // Enables browser globals like document, localStorage
+        es2021: true,
+        node: true,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/prop-types': 'off', // Disable prop-types if not used
+      'react/react-in-jsx-scope': 'error', // Enforce React import for JSX (React < 17)
+      quotes: ['error', 'double'], // Enforce double quotes
+      semi: ['error', 'always'], // Enforce semicolons
+      'no-unused-vars': 'warn', // Warn on unused variables
+      'no-undef': 'error', // Ensure undefined variables are caught
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect React version
+      },
     },
   },
-  plugins: ['react', 'react-hooks'],
-  rules: {
-    'react/prop-types': 'off', // Disable prop-types if not used
-    quotes: ['error', 'double'], // Enforce double quotes
-    semi: ['error', 'always'], // Enforce semicolons
-    'no-unused-vars': 'warn', // Optional: Warn on unused variables
-  },
-  settings: {
-    react: {
-      version: 'detect', // Automatically detect React version
-    },
-  },
-};
+];
